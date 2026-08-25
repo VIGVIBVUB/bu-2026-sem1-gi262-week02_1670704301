d7417b85a67385d5de0fa097e5ebfc6df6ada466
VIGVIBVUB <tharathip.chup@bumail.net>
2026-08-25 11:07:43 +0700

    เดวทำต่อนะ

--- Assets/Workshop/Student/Scripts/Lecture.cs
+++ Assets/Workshop/Student/Scripts/Lecture.cs
@@ -21,14 +21,37 @@ namespace Assignment
 
         #region Lecture
 
+        public string[] ironManSuit = new string[2];//เข้าถึงได้จากทุกที่ global
         public void LCT01_SyntaxArray()
         {
-            throw new System.NotImplementedException();
+            string[] _ironManSuit = new string[2];//ใช้ได้ในฟังชั่นนี้เท่านั้น local
+            //_ironManSuit[-1] index -1 บอกว่ายังค้นหาข้อมูล
+            _ironManSuit[0] = "Mark I";//set
+            _ironManSuit[1] = "Mark II";
+            //_ironManSuit[1] = 'a'; ใส่ข้อมูลให้ถูก
+            //_ironManSuit[2] = "Mark III"; ห้ามเกิน
+            string tonyStarkWear = _ironManSuit[0];//get
+            Debug.Log("TonyStark Wear" + tonyStarkWear);
+            Debug.Log("Room size: " + _ironManSuit.Length);
+            //เข้าถึงขนาดของ Array เพื่อใช้งานร่วมกับ for
+            Debug.Log(_ironManSuit[0]);
+            Debug.Log(_ironManSuit[1]);
+
         }
 
         public void LCT02_ArrayInitialize()
         {
-            throw new System.NotImplementedException();
+            string[] spidermanSuits = new string[] { "Classic", "Black Suit", "Iron Spider" };
+            string[] batmanSuits = new string[2] { "Classic batman", "White bat" };
+
+            Debug.Log($"Room size: {spidermanSuits.Length}");
+            Debug.Log(spidermanSuits[0]);
+            Debug.Log(spidermanSuits[1]);
+            Debug.Log(spidermanSuits[2]);
+
+            Debug.Log($"Room size: {batmanSuits.Length}");
+            Debug.Log(batmanSuits[0]);
+            Debug.Log(batmanSuits[1]);
         }
 
         /*
@@ -47,7 +70,17 @@ namespace Assignment
          */
         public void LCT03_SyntaxLoop()
         {
-            throw new System.NotImplementedException();
+            for (int i = 0; i < 10; i++)
+            {
+                Debug.Log("<10 : " + i);
+            }
+
+            Debug.Log("======================");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Debug.Log("<=10 : " + i);
+            }
         }
 
         /*
@@ -67,7 +100,7 @@ namespace Assignment
          * Mark I
          * Mark II
          * Mark III
-         * ====== Log by Two incrementer ======
+         * ====== Log by Two incrementer ======s
          * Mark I
          * Mark III
 
@@ -79,7 +112,14 @@ namespace Assignment
         public string[] lct04_ironManSuitNames;
         public void LCT04_LoopAndArray()
         {
-            throw new System.NotImplementedException();
+            Debug.Log("====== Log by One increnent ======");
+            for (int i = 0; i < lct04_ironManSuitNames.Length; i++) { //เพิ่มทีละ 1
+                Debug.Log(lct04_ironManSuitNames[i]);
+            }
+            Debug.Log("====== Log by Two increnent ======");
+            for (int i = 0; i < lct04_ironManSuitNames.Length; i++) { //เพิ่มทีละ 2
+                Debug.Log(lct04_ironManSuitNames[i]);
+            }
         }
 
         /*
@@ -113,8 +153,24 @@ namespace Assignment
          * int[,] my2DArray = new int[3, 5];
          */
         public void LCT05_Syntax2DArray()
-        {
-            throw new System.NotImplementedException();
+        {//col 1 2 3
+            int[,] my2DArray = new int[3, 3]  
+            { 
+                {1,2,3}, //row 1 0
+                {4,5,6}, //row 2 1
+                {7,8,9}, //row 3 2
+            };
+            //GetLenght(0) หมายถึง AArray ช่องแรกหรือ X หรือ col
+            for (int row = 0; row < my2DArray.GetLength(0); row++)
+            {
+                string rowStr = "";
+                //GetLenght(1) หมายถึง AArray ช่องแรกหรือ Y หรือ col
+                for (int col = 0; col < my2DArray.GetLength(1); col++)
+                {
+                    rowStr += my2DArray[row, col];
+                }
+                Debug.Log(rowStr);
+            }
         }
 
         /*
@@ -144,6 +200,7 @@ namespace Assignment
          * - lct06_my2DArray: อาร์เรย์ 2 มิติ (2D array) แก้ไขค่าได้จาก Inspector
          */
         [Header("LCT06_SizeOf2DArray")]
+        public int[,] LCT062DARRAY; //จะไม่แสดงข้อมูลใน unity
         public Grid2DInt lct06_my2DArray = new Grid2DInt
         {
             rows = 3,
@@ -153,6 +210,15 @@ namespace Assignment
         public void LCT06_SizeOf2DArray()
         {
             int[,] my2DArray = lct06_my2DArray.Get2DArray();
+
+            int rows = my2DArray.GetLength(0);
+            int cols = my2DArray.GetLength(1);
+            int size = my2DArray.GetLength(2);
+
+            Debug.Log("rows " + rows);
+            Debug.Log("cols " + cols);
+            Debug.Log("size " + size);
+
         }
 
         /*
@@ -238,7 +304,8 @@ namespace Assignment
         }
 
         #endregion
-
+        //public void LCT01_SyntaxArray()
+              
         private void PrintBoard(string[,] board)
         {
             StringBuilder sb = new();
--- Assets/Workshop/Student/Scripts/MapGenerator.cs
+++ Assets/Workshop/Student/Scripts/MapGenerator.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace Workshop.Student
 {
@@ -29,17 +30,68 @@ namespace Workshop.Student
 
             // 2. create obstacles
 
-            // 3. create floor
+            // 3. create floor 
+            for (int y = 0; y < rows; y++)
+            {
+                for (int x = 0; x < columns; x++)
+                {
+                    int r = UnityEngine.Random.Range(0, floorTiles.Length);
+                    GameObject tile = Instantiate(floorTiles[r], new Vector2(x, y), Quaternion.identity);
+                    tile.name = "Floor" + x + " " + y;
+                }
+            }
 
             // 4. create walls
-
+            for (int y = -1; y < rows + 1; y++)
+            {
+                for (int x = -1; x < columns + 1; x++)
+                {
+                    if (x == -1 || x == columns || y == -1 || y == rows)
+                    {
+                        int r = UnityEngine.Random.Range(0, wallTiles.Length);
+                        GameObject tile = Instantiate(wallTiles[r], new Vector2(x, y), Quaternion.identity);
+                        tile.name = "Wall" + x + " " + y;
+                    }
+                }
+            }
             // 5. random foods
 
+            int numberOfFood = UnityEngine.Random.Range(1, 3);
+            for (int i = 0; i < numberOfFood; i++)
+            {
+                int x_Food = UnityEngine.Random.Range(0, columns);
+                int y_Food = UnityEngine.Random.Range(0, rows);
+
+                int r = UnityEngine.Random.Range(0, floorTiles.Length);
+                Instantiate(foodTiles[0], new Vector2(x_Food, y_Food), Quaternion.identity);
+
+            }
+
             // 6. generate item along with the saveItemMap
+
+            for (int y = 0; y < saveItemMap.GetLength(0); y++)
+            {
+                for (int x = 0; x < saveItemMap.GetLength(1); y++)
+                {
+                    string item = saveItemMap[x, y];
+
+                    if (!string.IsNullOrEmpty(item))
+
+                        foreach (var foodTile in foodTiles)
+                        {
+                            if (foodTile.name == item)
+                            {
+                                GameObject food = Instantiate(foodTile, new Vector2(x, y), Quaternion.identity);
+                                food.name = item;
+                                break;
+                            }
+                        }
+
+                }
+            }
 
             // 7. place exit
 
         }
     }
-
 }
